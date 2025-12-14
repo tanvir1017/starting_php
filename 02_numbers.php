b84ef518773460584c7c485be31459f264d3f9a9
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-12-15 00:03:58 +0600

    🎸 Add: Enhance arithmetic operations with additional echo statements for clarity

--- 02_numbers.php
+++ 02_numbers.php
@@ -5,7 +5,49 @@
 $x = 30;
 $y = 10;
 
+echo "Addition with $x and $y" . "</br>";
 echo $x + $y;
+echo "</br>";
+echo "</br>";
+
+echo "Subtracting from $x by $y" . "</br>";
+echo $x - $y;
+echo "</br>";
+echo "</br>";
+
+echo "Multiplying the $x by $y" . "</br>";
+echo $x * $y;
+echo "</br>";
+echo "</br>";
+
+echo "Divide" . "</br>";
+echo $x / $y;
+echo "</br>";
+echo "</br>";
+
+echo "Reminder" . "</br>";
+echo $x % $y;
+echo "</br>";
+echo "</br>";
+
+
+echo "Power the $x by $y" . "</br>";
+echo $x ** $y;
+echo "</br>";
+echo "</br>";
+
+
+echo "Incrementing the value of $x by one" . "</br>";
+$x++;
+echo $x;
+echo "</br>";
+echo "</br>";
+
+
+echo "Decrementing the value of $y by one" . "</br>";
+$y--;
+echo $y;
+echo "</br>";
 echo "</br>";
 
 
