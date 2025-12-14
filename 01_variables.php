974b86b45e65ab76ee0f101ab19ce0bbbae7030d
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-12-14 22:53:10 +0600

    Add initial PHP files with variable declarations and HTML structure

---
+++ 01_variables.php
@@ -0,0 +1,35 @@
+<?
+
+    // Single line comment in PHP
+    # Another single line comment
+    /*
+        This is a multi-line comment in PHP
+        It can span multiple lines.
+    */
+
+    /**
+     * Variables in PHP
+     * A variable in PHP starts with a dollar sign ($) followed by the name of the variable.
+     * A variable is used to store data, such as numbers, strings, arrays, or objects.
+     * @param mixed $name
+    */
+
+    /**
+     * Data types in PHP
+     * PHP supports several data types including:
+     * - String: A sequence of characters enclosed in single or double quotes.
+     * - Integer: Whole numbers without a decimal point.
+     * - Float (or Double): Numbers with a decimal point.
+     * - Boolean: Represents two possible values: true or false.
+     * - Array: A collection of values stored in a single variable.
+     * - Object: An instance of a class that can contain properties and methods.
+      */
+
+
+
+    // Variable declaration and initialization
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    echo "Hello, Ayesha! <br />";
+      echo "Hello, WORLD! <br />";
+      echo 'Hello'. " Concatenation". '<br />' . "Is a fun <br />";
+    
+    ?>
+  </body>
+</html>
